cfbf9be6f4df33cab21cda92bc326eaab516d111
murilo-scapim <mhscapim@gmail.com>
2026-03-16 15:12:27 -0300

    Adiciona código do servo motor

---
+++ servo/servo.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+Servo servo;
+#define potPin A0 // Pino do potenciômetro
+int valor;
+
+void setup() {
+  servo.attach(9); // Configura o pino D9 - pino de controle do servo
+}
+
+void loop() {
+  valor = analogRead(potPin); // Leitura da tensão do pino A0
+  valor = map(valor, 0, 1023, 0, 179); // Converte a leitura em número (0-179)
+  servo.write(valor); // Controle PWM do servo
+  delay(15); // Atraso de 15 milissegundos
+}
